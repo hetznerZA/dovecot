ea7a7be52625e7dc06516ed84e430f5f74c1d61c
Geoff Robertson <gr@hetzner.co.za>
2018-08-08 15:56:35 +0200

    Correct the section

--- manifests/pop3.pp
+++ manifests/pop3.pp
@@ -7,7 +7,7 @@ class dovecot::pop3 (
   dovecot::config::dovecotcfmulti { 'pop3':
     config_file => 'conf.d/20-pop3.conf',
     changes     => [
-      "set protocol[ . = \"imap\"]/mail_plugins \"${mail_plugins} zlib\"",
+      "set protocol[ . = \"pop3\"]/mail_plugins \"${mail_plugins} zlib\"",
       "set pop3_uidl_format ${pop3_uidl_format}"
       ],
   }
